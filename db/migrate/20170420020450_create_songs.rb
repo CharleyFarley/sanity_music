17f461fe6cccf5507b126e8b3127be297fe95181
charles <charlesrichmondsmith@gmail.com>
2017-04-20 15:55:16 +1000

    date wont work

--- db/migrate/20170420020450_create_songs.rb
+++ db/migrate/20170420020450_create_songs.rb
@@ -2,7 +2,7 @@ class CreateSongs < ActiveRecord::Migration[5.0]
   def change
     create_table :songs do |t|
       t.string :name
-      t.date :year
+      t.integer :year
       t.string :artist
       t.integer :genre_id
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -12,3 +12,20 @@ genre = [
 genre.each do |genre|
 	Genre.create genre
 end
+
+song = [
+
+{ name: "whole lotta love", year: 2000 },
+{ name: "if i can turn back time", year: 2000},
+{ name: "thunderstruck", year: 2000},
+{ name: "itchy and scratchy", year: 2000},
+{ name: "Barbie Girl", year: 2000}
+
+]
+
+song.each do |song|
+  Song.create song 
+end
+
+
+
